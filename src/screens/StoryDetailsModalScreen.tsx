bec4785f65d86697bf2487bb16b1a493239c1f7a
victor.longon <vhlongon@gmail.com>
2022-01-04 21:33:45 +0100

    add styles for StoryDetails

--- src/screens/StoryDetailsModalScreen.tsx
+++ src/screens/StoryDetailsModalScreen.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { RouteProp, useRoute } from '@react-navigation/native';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  ScrollView,
+} from 'react-native';
 import { useStoryByIdQuery } from '../graphql/generated/graphql-types';
 import { RootStackParamsList } from '../types';
 
@@ -36,13 +42,13 @@ const StoryDetailsModalScreen = () => {
     );
   }
   return (
-    <View>
-      <Text>Title: {data.story?.title}</Text>
-      <Text>Summary: {data.story?.summary}</Text>
-      <Text>Author: {data.story?.author}</Text>
-      <Text>Text: {data.story?.text}</Text>
-      <Text>BookmarkId: {data.story?.bookmarkId}</Text>
-    </View>
+    <ScrollView style={styles.scrollView}>
+      <Text style={styles.summary}>{data.story?.summary}</Text>
+      <View style={styles.divider} />
+      <Text style={styles.text}>{data.story?.text}</Text>
+      <View style={styles.divider} />
+      <Text style={styles.author}>{data.story?.author}</Text>
+    </ScrollView>
   );
 };
 
@@ -52,6 +58,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  divider: {
+    marginVertical: 10,
+    height: 1,
+    backgroundColor: 'darkgrey',
+  },
+  scrollView: {
+    padding: 20,
+  },
+  author: {
+    fontStyle: 'italic',
+    fontSize: 16,
+    color: 'grey',
+    marginBottom: 20,
+  },
+  summary: {
+    fontSize: 20,
+    marginBottom: 20,
+    lineHeight: 24,
+    textAlign: 'justify',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 24,
+    textAlign: 'justify',
   },
 });
 
